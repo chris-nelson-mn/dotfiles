7c92bc88d2b172aacccd7a28c646359fc022f63a
Chris Nelson <chris.nelson.mn@gmail.com>
2018-04-03 14:52:32 -0500

    updating atom settings, aliases, add ngrok

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -29,3 +29,4 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gclean='gco master && git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
